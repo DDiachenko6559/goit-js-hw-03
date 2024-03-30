c084049a906ea8ab2e8e0791bc2b3a67fa0be9db
DDiachenko6559 <153743209+DDiachenko6559@users.noreply.github.com>
2024-03-30 11:37:57 +0200

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,19 @@
-// function checkForSpam(message) {
-//   const messageLow = message.toLowerCase();
-//   const sale = messageLow.includes('sale');
-//   const spam = messageLow.includes('spam');
-//   if (sale || spam) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function filterArray(numbers, value) {
+  let massive = [];
+  let number = 0;
 
-// console.log(checkForSpam('Latest technology news')); // false
-// console.log(checkForSpam('JavaScript weekly newsletter')); // false
-// console.log(checkForSpam('Get best sale offers now!')); // true
-// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-// console.log(checkForSpam('Trust me, this is not a spam message')); // true
-// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+  for (let i = 0; i < numbers.length; i += 1) {
+    number = numbers[i];
+    if (number > value) {
+      massive.push(`${number}`);
+    }
+  }
+
+  return massive;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
